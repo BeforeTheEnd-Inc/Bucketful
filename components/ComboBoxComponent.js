687b0b51f8d01cf1d94b6fe287ee58a8586010bb
omcdanie <omcdaniel1@gmail.com>
2018-03-28 19:16:14 -0500

    Fix up comments.

--- components/ComboBoxComponent.js
+++ components/ComboBoxComponent.js
@@ -9,23 +9,21 @@ export default class ComboBox extends Component {
         return(
             <div>
                 <label
-                    {/*the label cannot be null or an empty string*/}
+                    //the label cannot be null or an empty string
                     className=""
                     label=""
                 />
                 <input
-                    {/*
-                        className and placeholder text will should be standard everywhere except for the
-                        registration form.
-                    */}
+
+                //className and placeholder text will should be standard everywhere except for the registration form
                     className={}
                     placeholder={}
                     name='searchName'
                     type='search'
                 />
                 <select>
-                    {/*option should be dynamically rendered based on the stored list value*/}
-                    {/*if the list isn't empty then automatically select the first item in the list*/}
+                    <!--option should be dynamically rendered based on the stored list value
+                    if the list isn't empty then automatically select the first item in the list-->
                     <option value={}>value</option>
                 </select>
             </div>
