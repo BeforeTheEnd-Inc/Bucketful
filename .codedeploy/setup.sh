c18289c0beff11cc9361c804ecb57d4114887744
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-08 16:38:19 -0500

    added install docker commands to the setup script

--- .codedeploy/setup.sh
+++ .codedeploy/setup.sh
@@ -4,4 +4,7 @@ echo `date '+%Y-%m-%d %H:%M:%S'` >> /var/log/deploydates.log
 # install meteor
 curl "https://install.meteor.com/?release=1.6.1" | /bin/sh
 
+# install docker and docker-compose
+sudo yum install docker
+sudo pip install docker-compose
 
