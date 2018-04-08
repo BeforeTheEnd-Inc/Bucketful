273c64a34f2f37d0a9d67c439c50f023183cf985
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-08 17:26:50 -0500

    added permissions to the appspec file

--- .codedeploy/setup.sh
+++ .codedeploy/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo `date '+%Y-%m-%d %H:%M:%S'` >> /var/log/deploydates.log
 
+# Create
+mkdir -p /home/ec2-user/bucketful
+
 # install meteor
 curl "https://install.meteor.com/?release=1.6.1" | /bin/sh
 
