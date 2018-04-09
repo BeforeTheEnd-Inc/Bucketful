f0c7c525d5154543d80fe58f804f3c8c987062d8
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-08 19:56:37 -0500

    trying to get the git branch var to work with travis

--- bucketful-build.sh
+++ bucketful-build.sh
@@ -3,20 +3,22 @@
 # This will cause the shell to exit immediately if a simple command exits with a nonzero exit value.
 set -e
 
+echo $TRAVIS_BRANCH
 
-export BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+if [ -z "$TRAVIS_BRANCH" ]; then
+    export BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+else
+    export BRANCH=$TRAVIS_BRANCH
 
+fi
+
+echo "=> Running tests with mocha on  *$BRANCH* branch..."
+meteor test --once --driver-package meteortesting:mocha
 echo ""
 echo "=> Starting build for the *$BRANCH* branch..."
-echo ""
-echo "=> building meteor bundle ..."
 meteor build --server-only --directory .build
-echo ""
 echo "=> building docker image ..."
 docker build -t beforetheend/bucketful:$BRANCH .
-echo ""
 echo "=> start docker compose ..."
 docker-compose up -d
-echo ""
 echo "=> Hip hip hooray the app is running at: http://localhost:3030/"
-echo ""
