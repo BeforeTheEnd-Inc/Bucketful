a15495befded23842ca9b3170b2c468670166ffd
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-08 20:42:31 -0500

    added a var to the bucketful-build.sh that checks if the script is running in a local or travis env

--- bucketful-build.sh
+++ bucketful-build.sh
@@ -3,22 +3,29 @@
 # This will cause the shell to exit immediately if a simple command exits with a nonzero exit value.
 set -e
 
-echo $TRAVIS_BRANCH
-
 if [ -z "$TRAVIS_BRANCH" ]; then
     export BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+    export LOCAL=true
 else
     export BRANCH=$TRAVIS_BRANCH
+    export LOCAL=false
 
 fi
 
 echo "=> Running tests with mocha on  *$BRANCH* branch..."
 meteor test --once --driver-package meteortesting:mocha
-echo ""
+
+
 echo "=> Starting build for the *$BRANCH* branch..."
 meteor build --server-only --directory .build
+
+
 echo "=> building docker image ..."
 docker build -t beforetheend/bucketful:$BRANCH .
-echo "=> start docker compose ..."
-docker-compose up -d
-echo "=> Hip hip hooray the app is running at: http://localhost:3030/"
+
+
+if [ "$LOCAL" = true ]; then
+    echo "=> start docker compose ..."
+    docker-compose up -d
+    echo "=> Hip hip hooray the app is running at: http://localhost:3030/"
+else
