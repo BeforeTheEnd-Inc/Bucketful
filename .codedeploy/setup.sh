3253369b1fb7d8676ef8348b4ae9abbacf3bc25e
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-09 18:50:20 -0500

    fix typo in DEPLOYMENT_GROUP_NAME var

--- .codedeploy/setup.sh
+++ .codedeploy/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo `date '+%Y-%m-%d %H:%M:%S'` >> /home/ec2-user/deploydates.log
-echo "deploying to $DEPLOTMENT_GROUP_NAME Group" >> /home/ec2-user/deploydates.log 
+echo "deploying to $DEPLOYMENT_GROUP_NAME Group" >> /home/ec2-user/deploydates.log 
 
 # create
 mkdir -p /home/ec2-user/bucketful
