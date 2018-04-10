5cc07cdae067f16e577fee53e8c18913acde243f
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-10 15:08:51 -0500

    added a PORT env variable to the start_bucketful.sh script

--- .codedeploy/start_bucketful.sh
+++ .codedeploy/start_bucketful.sh
@@ -4,5 +4,16 @@ echo `date '+%Y-%m-%d %H:%M:%S'` - start_bucketful.sh - DEPLOYMENT_GROUP_NAME is
 
 sed -i "s/IGIVEUP_THISISAHACK/$DEPLOYMENT_GROUP_NAME/g" /home/ec2-user/bucketful/.codedeploy/docker-compose.yaml
 
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
+then
+    export PORT=3030
+elif [ "$DEPLOYMENT_GROUP_NAME" == "master" ]
+then
+    export PORT=80
+fi
+
+
+
 # run docker compose
 sudo /usr/local/bin/docker-compose --file /home/ec2-user/bucketful/.codedeploy/docker-compose.yaml up -d
