0630fe414d77249e3001b924fb3cc0fa517b968c
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-09 23:57:34 -0500

    adding in a hacky way of making sure the right docker image is deployed

--- .codedeploy/start_bucketful.sh
+++ .codedeploy/start_bucketful.sh
@@ -2,6 +2,7 @@
 
 echo `date '+%Y-%m-%d %H:%M:%S'` - start_bucketful.sh - DEPLOYMENT_GROUP_NAME is [$DEPLOYMENT_GROUP_NAME] >> /home/ec2-user/deploydates.log
 
+sed -i "s/IGIVEUP_THISISAHACK/$DEPLOYMENT_GROUP_NAME/g" /home/ec2-user/bucketful/.codedeploy/docker-compose.yaml
 
 # run docker compose
 sudo /usr/local/bin/docker-compose --file /home/ec2-user/bucketful/.codedeploy/docker-compose.yaml up -d
