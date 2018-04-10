c543578571f39cdf9c207bc89afe333d3f32be84
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-09 23:12:46 -0500

    added better logging for codedeploy scripts

--- .codedeploy/start_bucketful.sh
+++ .codedeploy/start_bucketful.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+echo `date '+%Y-%m-%d %H:%M:%S'` [start_bucketful.sh]  DEPLOYMENT_GROUP_NAME is $DEPLOYMENT_GROUP_NAME >> /home/ec2-user/deploydates.log
+
+
 # run docker compose
 sudo /usr/local/bin/docker-compose --file /home/ec2-user/bucketful/.codedeploy/docker-compose.yaml up -d
