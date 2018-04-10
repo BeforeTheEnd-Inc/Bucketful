39119b0941ae067a4cee783e17c8681a107c22e6
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-10 15:24:23 -0500

    attemtping to get all envs to run on a single ec2 instance for we don't go over the aws free tier for this project

--- .codedeploy/setup.sh
+++ .codedeploy/setup.sh
@@ -3,8 +3,8 @@ echo `date '+%Y-%m-%d %H:%M:%S'` - setup.sh - starting aws codedeploy >> /home/e
 echo `date '+%Y-%m-%d %H:%M:%S'` - setup.sh - deploying to [$DEPLOYMENT_GROUP_NAME] group >> /home/ec2-user/deploydates.log
 
 # create bucketful directory 
-mkdir -p /home/ec2-user/bucketful
-chown -R ec2-user:ec2-user /home/ec2-user/bucketful
+mkdir -p /home/ec2-user/bucketful_groups/$DEPLOYMENT_GROUP_NAME
+chown -R ec2-user:ec2-user /home/ec2-user/bucketful_groups/$DEPLOYMENT_GROUP_NAME
 
 # install meteor
 curl "https://install.meteor.com/?release=1.6.1" | /bin/sh
