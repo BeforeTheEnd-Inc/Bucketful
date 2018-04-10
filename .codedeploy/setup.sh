5f2fe95bd77e597db77489054ac3477dde76b6bb
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-09 19:21:54 -0500

    added BRANCH evn var based on DEPLOYMENT_GROUP_NAME and started the docker deamon

--- .codedeploy/setup.sh
+++ .codedeploy/setup.sh
@@ -2,6 +2,8 @@
 echo `date '+%Y-%m-%d %H:%M:%S'` >> /home/ec2-user/deploydates.log
 echo "deploying to $DEPLOYMENT_GROUP_NAME Group" >> /home/ec2-user/deploydates.log 
 
+export $BRANCH=$DEPLOYMENT_GROUP_NAME
+
 # create bucketful directory 
 mkdir -p /home/ec2-user/bucketful
 chown -R ec2-user:ec2-user /home/ec2-user/bucketful
@@ -12,6 +14,9 @@ curl "https://install.meteor.com/?release=1.6.1" | /bin/sh
 # install docker and docker-compose
 sudo yum -y install docker
 sudo pip install docker-compose
+
+# start docker daemon
+sudo service docker start
 
 # run docker compose
 docker-compose up -d
