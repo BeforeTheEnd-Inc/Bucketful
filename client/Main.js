daa702fd10c4fc034e4ab7bf73ab1607e5d868ed
David Para <david.para@avtechcorp.com>
2018-04-10 21:00:08 -0500

    hotfix: profile loading

--- client/Main.js
+++ client/Main.js
@@ -3,7 +3,7 @@ import {Tracker} from 'meteor/tracker';
 import { render } from 'react-dom';
 import React from 'react';
 
-import Index from "../imports/components/IndexComponent";
+import Index from "./IndexComponent";
 
 Meteor.startup(() => {
     Tracker.autorun(() => {
