933bcec7b1f5f79771d72180d0fff0347df8b52d
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-10 21:31:57 -0500

    For each deploy I forgot that you needed to being the docker containers down and then update the image then bring them backup

--- .codedeploy/start_bucketful.sh
+++ .codedeploy/start_bucketful.sh
@@ -4,5 +4,12 @@ echo `date '+%Y-%m-%d %H:%M:%S'` - start_bucketful.sh - DEPLOYMENT_GROUP_NAME is
 
 sed -i "s/IGIVEUP_THISISAHACK/$DEPLOYMENT_GROUP_NAME/g" /home/ec2-user/bucketful/.codedeploy/docker-compose.yaml
 
+
+# stop current docker containers
+sudo /usr/local/bin/docker-compose --file /home/ec2-user/bucketful/.codedeploy/docker-compose.yaml down
+
+# update image 
+sudo docker pull beforetheend/bucketful:$DEPLOYMENT_GROUP_NAME
+
 # run docker compose
 sudo /usr/local/bin/docker-compose --file /home/ec2-user/bucketful/.codedeploy/docker-compose.yaml up -d
