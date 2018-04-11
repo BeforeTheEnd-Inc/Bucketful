e361f7fb067ab2a470de5d15a316c0ec1c8bcdf5
nsstanley51 <nstanle2@mail.depaul.edu>
2018-04-11 02:11:56 -0500

    giving up on the source file method  and trying simple templating

--- .codedeploy/start_bucketful.sh
+++ .codedeploy/start_bucketful.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-source .codedeploy/env.sh
 
 echo `date '+%Y-%m-%d %H:%M:%S'` - `basename "$0"` - starting aws codedeploy >> /home/ec2-user/deploydates.log
 echo `date '+%Y-%m-%d %H:%M:%S'` - `basename "$0"` - deploying to [$DEPLOYMENT_GROUP_NAME] group >> /home/ec2-user/deploydates.log
-echo `date '+%Y-%m-%d %H:%M:%S'` - `basename "$0"` - PORT=$PORT , >> /home/ec2-user/deploydates.log
-echo `date '+%Y-%m-%d %H:%M:%S'` - `basename "$0"` - DOCKERCOMPOSEPATH=$DOCKERCOMPOSEPATH >> /home/ec2-user/deploydates.log
-
-
 echo `date '+%Y-%m-%d %H:%M:%S'` - start_bucketful.sh - DEPLOYMENT_GROUP_NAME is [$DEPLOYMENT_GROUP_NAME] >> /home/ec2-user/deploydates.log
 
-sed -i "s/IGIVEUP_THISISAHACK/$DEPLOYMENT_GROUP_NAME/g" /home/ec2-user/bucketful_groups/$DEPLOYMENT_GROUP_NAME/.codedeploy/docker-compose.yaml
-
-
-# if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
-# then
-#     export PORT=3030
-# elif [ "$DEPLOYMENT_GROUP_NAME" == "master" ]
-# then
-#     export PORT=80
-# elif [ "$DEPLOYMENT_GROUP_NAME" == "codedeploy" ]
-# then
-#     export PORT=4000
-# fi
+if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
+then
+    PORT=3030
+elif [ "$DEPLOYMENT_GROUP_NAME" == "master" ]
+then
+    PORT=80
+elif [ "$DEPLOYMENT_GROUP_NAME" == "codedeploy" ]
+then
+    PORT=4000
+fi
 
 echo `date '+%Y-%m-%d %H:%M:%S'` - start_bucketful.sh - PORT value set to [ $PORT ] >> /home/ec2-user/deploydates.log
 
@@ -30,7 +22,9 @@ cp -rp /home/ec2-user/bucketful_groups/.holding/. /home/ec2-user/bucketful_group
 rm -rf /home/ec2-user/bucketful_groups/.holding/.
 chown -R ec2-user:ec2-user /home/ec2-user/bucketful_groups/$DEPLOYMENT_GROUP_NAME
 
-
+# templating
+sed -i "s/DEPLOYMENT_GROUP_NAME/${DEPLOYMENT_GROUP_NAME}/g" /home/ec2-user/bucketful_groups/$DEPLOYMENT_GROUP_NAME/.codedeploy/docker-compose.yaml
+sed -i "s/PORT/${PORT}/g" /home/ec2-user/bucketful_groups/$DEPLOYMENT_GROUP_NAME/.codedeploy/docker-compose.yaml
 
 # stop current docker containers
 sudo /usr/local/bin/docker-compose --file /home/ec2-user/bucketful/.codedeploy/docker-compose.yaml down
