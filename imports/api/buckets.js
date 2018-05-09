1975fc7ae91c8f49a114c45620710fc1b8297ca4
Allison Diller <allison.diller@here.com>
2018-05-08 20:51:56 -0500

    removed commented out code

--- imports/api/buckets.js
+++ imports/api/buckets.js
@@ -15,33 +15,3 @@ Schema.Bucket = new SimpleSchema({
 });
 
 Buckets.attachSchema(Schema.Bucket);
-
-
-// const BucketSchema = new SimpleSchema({
-//     _id: Number,
-//     name: String,
-//     description: String,
-//     profileID: Number,
-//     status: String,
-//     image: String,
-//     category: String,
-//     progress: Number,
-//     tasks: [Tasks],
-//     costs: [Costs],
-// });
-
-
-// const BucketSchema = new SimpleSchema({
-//
-//   name: { type: String },
-//   description: { type: String },
-//   profileID: { type: Number},
-//   status: { type: String, defaultValue: 'Active'},
-//   image: { type: String, defaultValue: null},
-//   category: { type: String, defaultValue: null},
-//   progress: { type: String, defaultValue: null},
-//   tasks: {type: Array[Tasks]},
-//   costs: {type: Array[Costs]},
-// });
-
-// Buckets.attachSchema(BucketSchema);
